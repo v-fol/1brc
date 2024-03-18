dd9f1f5e40e087f0fb66a25d28da2d0f26828962
Volodymyr Foliush <volodymyr.foliush@traxgo.be>
2024-03-19 00:58:18 +0200

    [Iteration 3] Byte by byte (and say hello to mister defaultdict)

--- calculate_average.py
+++ calculate_average.py
@@ -1,7 +1,7 @@
 import time
+from collections import defaultdict
 
-
-stations_temps = dict()
+stations_temps = defaultdict(lambda: [float('inf'), float('-inf'), 0, 0])
 
 
 def timing_decorator(func):
@@ -25,22 +25,19 @@ def read_large_file(file_object):
 
 @timing_decorator
 def main():
-    with open("sample.txt", "r") as file:
+    with open("sample.txt", "rb") as file:
         gen = read_large_file(file)
         for line in gen:
-            station_name, temp = line.split(";")
+            station_name, temp = line.split(b";")
             temp = float(temp)
-            station = stations_temps.get(station_name)
-            if station:
-                station[0] = min(station[0], temp)
-                station[1] = max(station[1], temp)
-                station[2] += temp
-                station[3] += 1
-            else:
-                stations_temps[station_name] = [temp, temp, temp, 1]
+            station = stations_temps[station_name]
+            station[0] = min(station[0], temp)
+            station[1] = max(station[1], temp)
+            station[2] += temp
+            station[3] += 1
 
     answer = ", ".join(
-        f"{station_name}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
+        f"{station_name.decode()}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
         for station_name, station in sorted(stations_temps.items())
     )
 
