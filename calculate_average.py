df3ba9d7b19462af0598dd52f5bd57f11e2191ee
Volodymyr Foliush <volodymyr.foliush@traxgo.be>
2024-03-19 17:43:05 +0200

    [Iteration 4] Cpu go brrr (multiprocessing to the rescue)

--- calculate_average.py
+++ calculate_average.py
@@ -1,43 +1,92 @@
-import time
+import os, time
+
 from collections import defaultdict
-
-stations_temps = defaultdict(lambda: [float('inf'), float('-inf'), 0, 0])
-
-
-def timing_decorator(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        func(*args, **kwargs)
-        end = time.time()
-        print(f"Elapsed time: {end-start}")
-
-    return wrapper
+from multiprocessing import Pool, cpu_count
 
 
-def read_large_file(file_object):
-    """A generator to read a large file lazily."""
-    while True:
-        data = file_object.readline()
-        if not data:
-            break
-        yield data
+def get_file_chunks(file_name: str, cpu_count: int = 8) -> list:
+    file_size = os.path.getsize(file_name)
+    chunk_size = file_size // cpu_count
+    chunk = list()
+    with open(file_name, "r+b") as file:
+
+        def is_new_line(position):
+            if position == 0:
+                return True
+            else:
+                file.seek(position - 1)
+                return file.read(1) == b"\n"
+
+        def next_line(position):
+            file.seek(position)
+            file.readline()
+            return file.tell()
+
+        chunk_start = 0
+        while chunk_start < file_size:
+            chunk_end = min(file_size, chunk_start + chunk_size)
+            while not is_new_line(chunk_end):
+                chunk_end -= 1
+            if chunk_start == chunk_end:
+                chunk_end = next_line(chunk_end)
+            chunk.append(
+                (
+                    file_name,
+                    chunk_start,
+                    chunk_end,
+                )
+            )
+            chunk_start = chunk_end
+    return chunk
 
 
-@timing_decorator
-def main():
-    with open("sample.txt", "rb") as file:
-        gen = read_large_file(file)
-        for line in gen:
-            station_name, temp = line.split(b";")
+def process_chunk(chunk):
+    file_name, chunk_start, chunk_end = chunk
+    stations_temps = defaultdict(lambda: [float("inf"), float("-inf"), 0, 0])
+    with open(file_name, "r") as f:
+        f.seek(chunk_start)
+        for line in f:
+            chunk_start += len(line)
+            if chunk_start > chunk_end:
+                break
+            station_name, temp = line.split(";")
             temp = float(temp)
             station = stations_temps[station_name]
-            station[0] = min(station[0], temp)
-            station[1] = max(station[1], temp)
+            if station[0] > temp:
+                station[0] = temp
+            if station[1] < temp:
+                station[1] = temp
             station[2] += temp
             station[3] += 1
 
+    return dict(stations_temps)
+
+
+def main(filename: str, cpu_count: int = cpu_count()):
+    pool = Pool(cpu_count)
+
+    chunks = get_file_chunks(filename, cpu_count)
+    results = []
+    for chunk in chunks:
+        results.append(pool.apply_async(process_chunk, (chunk,)))
+    pool.close()
+    pool.join()
+
+    stations_temps = dict()
+    for result in results:
+        chunk_result = result.get()
+        for station_name, station_vals in chunk_result.items():
+            station = stations_temps.get(station_name)
+            if station:
+                station[0] = min(station[0], station_vals[0])
+                station[1] = max(station[1], station_vals[1])
+                station[2] += station_vals[2]
+                station[3] += station_vals[3]
+            else:
+                stations_temps[station_name] = station_vals
+
     answer = ", ".join(
-        f"{station_name.decode()}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
+        f"{station_name}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
         for station_name, station in sorted(stations_temps.items())
     )
 
@@ -45,4 +94,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    time_start = time.time()
+    main("sample.txt", cpu_count())
+    print(f"Elapsed time: {time.time()-time_start}")
