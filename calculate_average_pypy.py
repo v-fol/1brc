96e20767567d03ee744cf6ae6f8c2ee54270bd88
Volodymyr Foliush <volodymyr.foliush@traxgo.be>
2024-03-20 02:01:34 +0200

    [PyPy Iteration 1] Back to drawing board

---
+++ calculate_average_pypy.py
@@ -0,0 +1,107 @@
+import os, time
+
+from collections import defaultdict
+from multiprocessing import Pool, cpu_count
+
+
+def get_file_chunks(file_name: str, cpu_count: int = 8) -> list:
+    file_size = os.path.getsize(file_name)
+    chunk_size = file_size // cpu_count
+    chunk = list()
+    with open(file_name, "r+b") as file:
+
+        def is_new_line(position):
+            if position == 0:
+                return True
+            else:
+                file.seek(position - 1)
+                return file.read(1) == b"\n"
+
+        def next_line(position):
+            file.seek(position)
+            file.readline()
+            return file.tell()
+
+        chunk_start = 0
+        while chunk_start < file_size:
+            chunk_end = min(file_size, chunk_start + chunk_size)
+            while not is_new_line(chunk_end):
+                chunk_end -= 1
+            if chunk_start == chunk_end:
+                chunk_end = next_line(chunk_end)
+            chunk.append(
+                (
+                    file_name,
+                    chunk_start,
+                    chunk_end,
+                )
+            )
+            chunk_start = chunk_end
+    return chunk
+
+
+def process_chunk(chunk):
+    file_name, chunk_start, chunk_end = chunk
+    stations_temps = defaultdict(lambda: [float("inf"), float("-inf"), 0, 0])
+    with open(file_name, "rb") as f:
+        f.seek(chunk_start)
+        remaining_bytes = chunk_end - chunk_start
+        line = f.readline()
+         
+        while True:
+            if remaining_bytes <= 0:
+                break
+            
+            station_and_temp = line.split(b";")
+            temp = float(station_and_temp[1])
+            station = stations_temps[station_and_temp[0].decode()]
+
+            if station[0] > temp:
+                station[0] = temp
+            if station[1] < temp:
+                station[1] = temp
+            station[2] += temp
+            station[3] += 1
+
+            remaining_bytes -= len(line)
+            line = f.readline()
+
+    return dict(stations_temps)
+
+
+def main(filename: str, cpu_count: int = cpu_count()):
+    pool = Pool(cpu_count)
+
+    chunks = get_file_chunks(filename, cpu_count)
+    results = []
+    for chunk in chunks:
+        results.append(pool.apply_async(process_chunk, (chunk,)))
+    pool.close()
+    pool.join()
+
+    stations_temps = dict()
+    for result in results:
+        chunk_result = result.get()
+        for station in chunk_result.items():
+            if stations_temps.get(station[0]) is None:
+                stations_temps[station[0]] = station[1]
+            else:
+                if station[1][0] < stations_temps[station[0]][0]:
+                    stations_temps[station[0]][0] = station[1][0]
+                if station[1][1] > stations_temps[station[0]][1]:
+                    stations_temps[station[0]][1] = station[1][1]
+                stations_temps[station[0]][2] += station[1][2]
+                stations_temps[station[0]][3] += station[1][3]
+
+    answer = ", ".join(
+        f"{station_name}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
+        for station_name, station in sorted(stations_temps.items())
+    )
+
+    print("{" + answer + "}")
+
+
+if __name__ == "__main__":
+    time_start = time.time()
+    main("sample.txt", cpu_count())
+    print(f"Elapsed time: {time.time()-time_start}")
