ffc55f4ff4ace3fcc6df70aaef69006366b0dd29
Volodymyr Foliush <volodymyr.foliush@traxgo.be>
2024-08-02 19:42:35 +0300

    [PyPy Iteration 5] Forget buffer, chunking chunks

--- calculate_average_mmap_pypy.py
+++ calculate_average_mmap_pypy.py
@@ -40,12 +40,41 @@ def get_file_chunks(file_name: str, cpu_count: int = 8) -> list:
     return chunk
 
 
-def process_chunk(chunk):
+def get_subchunks(start: int, end: int, file_name: str, devide_by: int) -> list:
+    chunk = list()
+    chunk_size = (end - start) // devide_by
+    with open(file_name, "rb") as file:
+
+        def is_new_line(position):
+            if position == 0:
+                return True
+            else:
+                file.seek(position - 1)
+                return file.read(1) == b"\n"
+
+        def next_line(position):
+            file.seek(position)
+            file.readline()
+            return file.tell()
+
+        chunk_start = start
+        while chunk_start < end:
+            chunk_end = min(end, chunk_start + chunk_size)
+            while not is_new_line(chunk_end):
+                chunk_end -= 1
+            if chunk_start == chunk_end:
+                chunk_end = next_line(chunk_end)
+            chunk.append(chunk_end - chunk_start)
+            chunk_start = chunk_end
+    return chunk
+
+
+def process_chunk(chunk) -> dict:
     file_name, chunk_start, chunk_end = chunk
     length = chunk_end - chunk_start
     new_chunk_start = chunk_start - (chunk_start % mmap.ALLOCATIONGRANULARITY)
     diff = chunk_start - new_chunk_start
-    stations_temps = defaultdict(lambda: [float("inf"), float("-inf"), 0, 0])
+    stations_temps = defaultdict(lambda: [100, -100, 0, 0])
     with open(file_name, "rb") as f:
         with mmap.mmap(
             f.fileno(),
@@ -53,27 +82,28 @@ def process_chunk(chunk):
             offset=new_chunk_start,
             access=mmap.ACCESS_READ,
         ) as mm:
+            mm.madvise(mmap.MADV_SEQUENTIAL)
             mm.seek(diff)
-            buffersize = 1024
-            incomplete_line = b''
-            chunk = mm.read(buffersize)
+            subchunks = get_subchunks(chunk_start, chunk_end, file_name, 20)
+            
+            for subchunk in subchunks:
+                buffer = mm.read(subchunk)
+                index = 0
+                while index < subchunk:
+                    semicolon = buffer.find(b";", index)
+                    newline = buffer.find(b"\n", semicolon)
 
-            while chunk:
-                lines = (incomplete_line + chunk).splitlines(True)
-                incomplete_line = lines.pop() if lines[-1][-1:] != b'\n' else b''
-                for line in lines:
-                    semicolon_pos = line.find(b';')
-                    temp = float(line[semicolon_pos + 1:].strip())
-                    station = stations_temps[line[:semicolon_pos].decode()]
+                    temp = float(buffer[semicolon + 1: newline].strip())
+                    station_vals = stations_temps[buffer[index:semicolon]]
+                    if temp < station_vals[0]:
+                        station_vals[0] = temp
+                    if temp > station_vals[1]:
+                        station_vals[1] = temp
+                    station_vals[2] += temp
+                    station_vals[3] += 1
+                    index = newline + 1
 
-                    if station[0] > temp:
-                        station[0] = temp
-                    if station[1] < temp:
-                        station[1] = temp
-                    station[2] += temp
-                    station[3] += 1
-                chunk = mm.read(buffersize)
-
+                
     return dict(stations_temps)
 
 
@@ -101,7 +131,7 @@ def main(filename: str, cpu_count: int = cpu_count()):
                 stations_temps[station_name] = station_vals
 
     answer = ", ".join(
-        f"{station_name}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
+        f"{station_name.decode()}={station[0]}/{station[2]/station[3]:.2f}/{station[1]}"
         for station_name, station in sorted(stations_temps.items())
     )
 
@@ -110,5 +140,5 @@ def main(filename: str, cpu_count: int = cpu_count()):
 
 if __name__ == "__main__":
     time_start = time.time()
-    main("sample.txt", cpu_count())
+    main("sample.txt", 16)
     print(f"Elapsed time: {time.time() - time_start}")
